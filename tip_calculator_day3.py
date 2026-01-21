fa167f06e5042b9aada096328a4250fbfd8ca90b
urshahaha <shresthaursha999@gmail.com>
2026-01-21 16:58:41 +0545

    Create tip_calculator_day3.py

---
+++ tip_calculator_day3.py
@@ -0,0 +1,10 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("no of people to split the bill? "))
+tip_percent = tip / 100
+total_tip = bill * tip_percent
+total_bill = bill + total_tip
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+print(f"Each person pays: ${final_amount}")
